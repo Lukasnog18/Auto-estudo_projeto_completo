fb93aa4c38d45d313f2c824fba7e6bdf0d4507bc
Lucas Nogueira <luksnogueira18@gmail.com>
2023-08-30 23:43:38 -0300

    Update Contato id.java

--- backend/Agenda/src/main/java/com/quixada/ufc/br/Agenda/model/Contato.java
+++ backend/Agenda/src/main/java/com/quixada/ufc/br/Agenda/model/Contato.java
@@ -17,7 +17,7 @@ public class Contato {
     // private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private UUID id;
 
     @NotNull
